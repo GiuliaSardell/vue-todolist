d4bf5d678a4821d4a98e9bf456f223f0118d5773
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-11-12 15:38:37 +0100

    stampa lista

---
+++ Milestone1/js/vue.js
@@ -0,0 +1,34 @@
+const app = new Vue({
+  el: '#app',
+
+  data:{
+    thingsToDo:[
+      {
+        text: 'Comprare il pare',
+        done: false 
+      },
+      {
+        text: 'Inviare il pacco',
+        done: false 
+      },
+      {
+        text: 'Pagare la bolletta della luce',
+        done: false 
+      },
+      {
+        text: 'Cercare regali di natale',
+        done: false 
+      },
+
+
+    ]
+ 
+    // che indica se il todo è stato fatto oppure no
+  },
+
+
+
+
+
+
+})
